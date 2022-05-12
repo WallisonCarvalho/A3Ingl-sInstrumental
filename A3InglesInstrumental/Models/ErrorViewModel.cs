5b2343d0b991fe13d1d76810216602f61ddfec52
Wallison Carvalho <wallison.ecarvalho95@gmail.com>
2022-05-12 15:48:06 -0300

    first commit

---
+++ A3InglesInstrumental/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace A3InglesInstrumental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
